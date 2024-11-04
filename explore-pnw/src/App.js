5a234b8843bb08217dbe4d0f3a6f40875665435a
cse-brendanmcnichols <mcnichob@email.sc.edu>
2024-11-03 23:28:02 -0500

    trying to fix 404 error on other pages

--- explore-pnw/src/App.js
+++ explore-pnw/src/App.js
@@ -12,10 +12,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/destinations" element={<Destinations />} />
-        <Route path="/space-needle" element={<SpaceNeedle />} />
-        <Route path="/schedule-planner" element={<SchedulePlanner />} />
-        <Route path="/travel-tips" element={<TravelTips />} />
+        <Route path="/" element={<Destinations />} />
+        <Route path="/" element={<SpaceNeedle />} />
+        <Route path="/" element={<SchedulePlanner />} />
+        <Route path="/" element={<TravelTips />} />
       </Routes>
     </Router>
   );
